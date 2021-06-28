d2a96d3d6bbb46584bbae6720926a38659a7714e
reuven9632 <reuven9632@gmail.com>
2021-06-28 09:08:30 +0300

    2.0 make validator

---
+++ src/main/java/com/example/demo/currency/CurrencyValidator.java
@@ -0,0 +1,37 @@
+package com.example.demo.currency;
+
+import java.util.function.Function;
+
+import static com.example.demo.currency.CurrencyValidator.CurrencyResult.*;
+
+public interface CurrencyValidator extends Function<Currency, CurrencyValidator.CurrencyResult> {
+
+    static CurrencyValidator isNameValid(){
+        return currency -> currency.getNameCurrency().length() > 2 ? SUCCESS : NAME_CURRENCY_NOT_VALID;
+    }
+
+    static CurrencyValidator isAmountOfCurrencyValid(){
+        return currency -> currency.getAmountOfCurrency() > 0 ? SUCCESS : AMOUNT_OF_CURRENCY_NOT_VALID;
+    }
+
+    static CurrencyValidator isValueOfCurrencyValid(){
+        return currency -> currency.getValueOfCurrency() > 0 ? SUCCESS : VALUE_OF_CURRENCY_NOT_VALID;
+    }
+
+    static CurrencyValidator and(CurrencyValidator other){
+        return currency -> {
+            CurrencyResult result = this.apply(currency);
+            return result.equals(SUCCESS) ? other.apply() : result;
+        };
+    }
+
+
+
+    enum CurrencyResult{
+        SUCCESS,
+        NAME_CURRENCY_NOT_VALID,
+        AMOUNT_OF_CURRENCY_NOT_VALID,
+        VALUE_OF_CURRENCY_NOT_VALID
+    }
+
+}
